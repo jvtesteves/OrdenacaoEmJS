fedbb748beb0aaa6a920cdc1295cc921334c116d
Joao Victor Tavares Esteves <joaovtesteves2002@gmail.com>
2023-02-03 10:07:51 -0300

    Codigos aula 01

--- Aula01/menorValor.js
+++ Aula01/menorValor.js
@@ -1,10 +1,10 @@
 const livros = require('./listaLivros');
 
 function menorValor(arrProdutos, posicaoInicial) {
-    let maisBarato = 0;
+    let maisBarato = posicaoInicial;
     
-    for (let atual = 0; atual < livros.length; atual++) {
-        if (livros[atual].preco < livros[maisBarato].preco) {
+    for (let atual = 0; atual < arrProdutos.length; atual++) {
+        if (arrProdutos[atual].preco < arrProdutos[maisBarato].preco) {
             maisBarato = atual;
         }
     }
