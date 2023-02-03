cacc8e67177f6a08332a149ffd8bad4f59d9b7fc
Joao Victor Tavares Esteves <joaovtesteves2002@gmail.com>
2023-02-03 09:37:34 -0300

    Codigos aula 01

---
+++ Aula01/listaLivros.js
@@ -0,0 +1,28 @@
+const precosLivros = [
+    {
+    nome: "JavaScript",
+    preco: 25
+    },
+    {
+    nome: "PHP",
+    preco: 15
+    }, 
+    {
+    nome: 'Java',
+    preco: 30
+    }, 
+    {
+    nome: 'Elixir',
+    preco: 50
+    }, 
+    {
+    nome: 'GO',
+    preco: 45
+    }, 
+    {
+    nome: 'Python',
+    preco: 20
+}
+];
+
+module.exports = precosLivros;
